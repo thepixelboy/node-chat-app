7fd4876672dd83659ba2897e8fcceb7c83684777
Ruben <34570952+thepixelboy@users.noreply.github.com>
2021-01-08 19:28:55 +0100

    Broadcasting Events

--- public/js/chat.js
+++ public/js/chat.js
@@ -4,10 +4,6 @@ socket.on('message', (message) => {
   console.log(message);
 });
 
-socket.on('sendMessage', (message) => {
-  console.log(message);
-});
-
 document.querySelector('#message-form').addEventListener('submit', (e) => {
   e.preventDefault();
 
